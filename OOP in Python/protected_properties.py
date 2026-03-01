ede3aee4dd1bc2cb12d47ad0d20d4342ff8a5256
FaysalMahmudSajan <faysalmahmud4300@gmail.com>
2026-03-01 14:29:21 +0600

    fix: Correct attribute access for salary in Person class and document AttributeError

--- OOP in Python/protected_properties.py
+++ OOP in Python/protected_properties.py
@@ -1,7 +1,17 @@
 class Person:
     def __init__(self,name,salary):
         self.name=name
-        self._salary=salary
+        self.__salary=salary
 
 p1 = Person('Ovi',65000)
-print(p1.name,p1._salary)
+print(p1.name,p1.__salary)
+
+
+# ---------------------------------------------------------------------------
+# AttributeError                            Traceback (most recent call last)
+# Cell In[4], line 7
+#       4         self.__salary=salary
+#       6 p1 = Person('Ovi',65000)
+# ----> 7 print(p1.name,p1.__salary)
+
+# AttributeError: 'Person' object has no attribute '__salary'
